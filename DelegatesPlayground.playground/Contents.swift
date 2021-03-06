4ededa1941dd449aa356a25f838663cdd825a29d
Anas AbuZaitoun <anasomarzaitoun@gmail.com>
2021-03-06 13:52:42 +0200

    Added delegates main points

--- DelegatesPlayground.playground/Contents.swift
+++ DelegatesPlayground.playground/Contents.swift
@@ -10,7 +10,26 @@ struct Cookie {
 }
 
 class Bakery {
+    
+    var delegate: BakeryDelegate?
+    
     func makeCookie(){
         var cookie = Cookie()
+        cookie.size = 7
+        cookie.hasChocolateChips = true
+        
+        delegate?.cookieWasBaked(cookie)
     }
 }
+
+class CookieShop: BakeryDelegate {
+    func cookieWasBaked(_ cookie: Cookie) {
+        print("Cookie is ready! \(cookie.hasChocolateChips ? "And it has chocolate chips!" : "It doesn't contain chips")")
+    }
+}
+
+let shop = CookieShop()
+let bakery = Bakery()
+bakery.delegate = shop
+
+bakery.makeCookie()
