3fed1226de92879aa9aa6943f283a3fad0c7c98b
Anas AbuZaitoun <anasomarzaitoun@gmail.com>
2021-03-06 13:32:19 +0200

    Added cookie struct, class and protocol

--- DelegatesPlayground.playground/Contents.swift
+++ DelegatesPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+protocol BakeryDelegate {
+    func cookieWasBaked(_ cookie: Cookie)
+}
+
+struct Cookie {
+    var size: Int = 5
+    var hasChocolateChips: Bool = false
+}
+
+class Bakery {
+    func makeCookie(){
+        var cookie = Cookie()
+    }
+}
